4739edb874219642f8f2043ef67ed5be9bc45c28
KhangTGR <khangtgr@gmail.com>
2022-11-07 17:00:50 +0700

    fix

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-# Your code is here
+docker-compose down
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker network prune
+docker volume prune
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-# Your code is here
+docker-compose up
